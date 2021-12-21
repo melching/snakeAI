5d575ae5a2aa74586410df8384fd2ff07fe0c08e
Christian Melching <20678978+melching@users.noreply.github.com>
2021-12-21 15:55:43 +0100

    Added game logic and changed some things.

--- snake.py
+++ snake.py
@@ -1,9 +1,9 @@
-import numpy as np
+# import numpy as np
+import random
 import pygame
 
 class Snake:
     '''
-    -1 = border
     0  = empty
     1  = snake body
     2  = snake head
@@ -14,43 +14,90 @@ class Snake:
         self.render = render
 
         self.state = "running"
-        self.score = 0
+        self.x_dim = x_dim
+        self.y_dim = y_dim
 
-        self.create_board(x_dim, y_dim)
+        self.snake_body = []
+        self.apple = None
+        self.empty = []
+        for x in range(x_dim):
+            for y in range(y_dim):
+                self.empty.append((x,y))
+
+        self.init_state(x_dim, y_dim)
 
         if self.render:
             pygame.init()
             self.screen = pygame.display.set_mode((800,800))
             self.draw_board()
 
-    def create_board(self, x_dim, y_dim):
+    def init_state(self, x_dim, y_dim):
         assert x_dim>2 and y_dim>2, "Board is to small!"
 
-        # create emtpy board
-        self.board = np.zeros(shape=(x_dim+2, y_dim+2), dtype=np.int8)
-        
-        # set boarders to -1
-        self.board[0,:] = -1
-        self.board[-1,:] = -1
-        self.board[:,0] = -1
-        self.board[:,-1] = -1
-
         # set snake head
-        self.board[x_dim//2 + 1, y_dim//2 + 1] = 2
+        head = (x_dim//2, y_dim//2)
+        self.snake_body.append(head)
+        self.empty.remove(head)
 
         # place apple
-        self.place_apple()
+        self.place_apple_randomly()
         
-    def place_apple(self,):
-        # get empty fields
-        where_empty = np.where(self.board==0)
-        rand_pos = np.random.randint(where_empty[0].shape[0])
-        self.board[where_empty[0][rand_pos], where_empty[1][rand_pos]] = 3
+    def place_apple_randomly(self,):
+        rand_pos = random.choice(self.empty)
+        self.apple = rand_pos
+        self.empty.remove(self.apple)
 
-    def move(self,direction):
+    def place_apple(self, x, y):
+        # removes old apple (manually for testing) and places new one
+        assert (x,y) not in self.snake_body
+        self.empty.append(self.apple)
+        self.apple = (x,y)
+        if self.render:
+            self.draw_board()
+
+    def move(self, direction):
         '''
-        0 = 
+        0 = up
+        1 = right
+        2 = down
+        3 = left
         '''
+        assert self.state is not "failed", "Can't move, Game already ended!"
+
+        diff_x = 0
+        diff_y = 0
+        if   direction == 0:
+            diff_y = -1
+        elif direction == 1:
+            diff_x = 1
+        elif direction == 2:
+            diff_y = 1
+        elif direction == 3:
+            diff_x = -1
+
+        nextpos = (self.snake_body[0][0] + diff_x, self.snake_body[0][1] + diff_y)
+
+        # check if next pos hits border or body
+        if nextpos[0] in [-1, self.x_dim] or nextpos[1] in [-1, self.y_dim] or nextpos in self.snake_body:
+            self.state = "failed"
+            return
+
+        # check if next pos is apple
+        if nextpos == self.apple:
+            # insert nextpos to snake
+            self.snake_body.insert(0, nextpos)
+            # place new apple
+            self.place_apple_randomly()
+
+        # check if next pos is empty
+        elif nextpos in self.empty:
+            # move snake
+            self.snake_body.insert(0,nextpos)
+            # remove last pos
+            self.empty.append(self.snake_body[-1])
+            self.snake_body = self.snake_body[:-1]
+
+        self.draw_board()
 
     def draw_board(self,):
         assert self.render == True, "Draw has been calles while render is False!"
@@ -59,7 +106,6 @@ class Snake:
         body_color  = (255,255,255)
         head_color  = (255,200,200)
         apple_color = (255,0,0)
-
         block_width = 15
 
         # draw board
@@ -67,40 +113,35 @@ class Snake:
         pygame.draw.rect(
             self.screen, 
             board_color, 
-            (0 , 0, (self.board.shape[0]-2)*block_width, (self.board.shape[1]-2)*block_width), 
+            (0 , 0, self.x_dim*block_width, self.y_dim*block_width), 
             0
         )
 
         # draw snake body and head
-        where_body = np.where(self.board==1)
-        for i in range(len(where_body[0])):
+        for x,y in self.snake_body[1:]:
             pygame.draw.rect(
                 self.screen, 
                 body_color, 
-                (where_body[0][i]*block_width , where_body[1][i]*block_width, block_width, block_width), 
+                (x*block_width , y*block_width, block_width, block_width), 
                 0
             )
-        where_head = np.where(self.board==2)
-        print(where_head)
         pygame.draw.rect(
             self.screen, 
             head_color, 
-            (where_head[0][0]*block_width , where_head[1][0]*block_width, block_width, block_width), 
+            (self.snake_body[0][0]*block_width , self.snake_body[0][1]*block_width, block_width, block_width), 
             0
         )
 
         # draw apple
-        where_apple = np.where(self.board==3)
         pygame.draw.rect(
             self.screen, 
             apple_color, 
-            (where_apple[0][0]*block_width , where_apple[1][0]*block_width, block_width, block_width), 
+            (self.apple[0]*block_width , self.apple[1]*block_width, block_width, block_width), 
             0
         )
 
         # show score
-        pygame.display.set_caption('snakeAI - Current Score: ' + str(self.score))
+        pygame.display.set_caption('snakeAI - Current Score: ' + str(len(self.snake_body)))
 
         # update window
         pygame.display.update()
-
--- test_draw.py
+++ test_draw.py
@@ -1,7 +1,18 @@
 from snake import Snake
 import time
+import pygame
 
 game = Snake(40, 40, render=True)
 
-a = 1
-# time.sleep(5)
+while game.state == "running":
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                game.move(0)
+            if event.key == pygame.K_RIGHT:
+                game.move(1)
+            if event.key == pygame.K_DOWN:
+                game.move(2)
+            if event.key == pygame.K_LEFT:
+                game.move(3)
