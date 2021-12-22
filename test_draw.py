17cc6aaba74550cd61f221eaccbaabf04349cd07
Christian Melching <20678978+melching@users.noreply.github.com>
2021-12-22 19:21:53 +0100

    Addes some prints.

--- test_draw.py
+++ test_draw.py
@@ -3,6 +3,7 @@ import time
 import pygame
 
 game = Snake(40, 40, render=True)
+print(len(game.empty))
 
 while game.state == "running":
     events = pygame.event.get()
@@ -10,9 +11,17 @@ while game.state == "running":
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 game.move(0)
+                print("Move Up")
             if event.key == pygame.K_RIGHT:
                 game.move(1)
+                print("Move Right")
             if event.key == pygame.K_DOWN:
                 game.move(2)
+                print("Move Down")
             if event.key == pygame.K_LEFT:
                 game.move(3)
+                print("Move Left")
+            print("Empty Fields", len(game.empty))
+            print("Snake", game.snake_body)
+
+print(game.state)
